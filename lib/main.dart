032777e869ae38a55af507d941772eec62c47170
nikita-grabek <nikita.grabek@magenta.de>
2022-05-27 15:40:38 +0200

    Updating Existing Notes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:success_planner/constants/routes.dart';
 import 'package:success_planner/services/auth/auth_service.dart';
 import 'package:success_planner/views/login_view.dart';
-import 'package:success_planner/views/notes/new_notes_view.dart';
+import 'package:success_planner/views/notes/create_update_note_view.dart';
 import 'package:success_planner/views/notes/notes_view.dart';
 import 'package:success_planner/views/register_view.dart';
 import 'package:success_planner/views/verify_email_view.dart';
@@ -20,7 +20,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
-        newNoteRoute: (context) => const NewNoteView(),
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
       home: const HomePage(),
     ),
